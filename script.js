dbc97861e192c2d7a81a726c901edc9d2a667e37
PetraCore <102828971+PetraCore@users.noreply.github.com>
2024-02-08 14:44:09 +0100

    Implement password validation in JS

--- script.js
+++ script.js
@@ -1,1 +1,63 @@
-console.log("connected.")
+const submitBtn = document.querySelector('#submit');
+const password = document.querySelector('#password');
+const confirmPassword = document.querySelector('#confirm-password');
+
+function blockButton(btn) {
+    btn.disabled = true;
+    btn.classList.add('error');
+}
+
+function unblockButton(btn) {
+    btn.disabled = false;
+    btn.classList.remove('error');
+}
+
+function clearError(...elements) {
+    unblockButton(submitBtn);
+
+    elements.forEach(element => {
+        element.classList.remove('error'); 
+    });
+}
+
+function clearSuccess(...elements) {
+    elements.forEach(element => {
+        element.classList.remove('success'); 
+    });
+}
+
+function raiseError(...elements) {
+    blockButton(submitBtn);
+
+    clearSuccess(...elements);
+    elements.forEach(element => {
+        element.classList.add('error'); 
+    });
+}
+
+function raiseSuccess(...elements) {
+    clearError(...elements);
+    elements.forEach(element => {
+        element.classList.add('success'); 
+    });
+}
+
+function validatePassword() {
+    if(!password.value) {
+        raiseError(password, confirmPassword);
+        return;
+    }
+    if(password.value !== confirmPassword.value) {
+        raiseError(password, confirmPassword);
+        return;
+    }
+
+    raiseSuccess(password, confirmPassword);
+}
+
+function setupFormInput() {
+    password.addEventListener('blur', validatePassword);
+    confirmPassword.addEventListener('blur', validatePassword); 
+}
+
+setupFormInput();
